687c3951ca7a6d3e80a4da9dcc22308086699c2a
Lucas Bauer <lucas.d.bauer95@gmail.com>
2023-05-02 19:24:24 -0500

    Remove title function

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,10 @@
-const title = (name) => {
-  // This factory will contain functions related to the title of the
-  // project or task.
-  const addTitle = (text) => {
-    // Add title to new todo
-    console.log(`Add is the title: ${text}`);
-  };
-
-  const getName = () => name;
-
-  const editTitle = (text) => {
-    console.log(`Edit title ${text}`);
-  };
-
-  return { addTitle, editTitle, getName };
-};
-
 const taskFactory = () => {
   const addTask = (titleText) => {
     const newTitle = title(titleText);
     newTitle.addTitle(titleText);
-    console.log(newTitle.getName());
   };
 
   const getTitle = () => "Test";
 
   return { addTask, getTitle };
 };
-
-const foo = taskFactory();
-
-foo.addTask("Magic");
