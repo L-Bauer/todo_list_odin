9326d9b4dc305e321d443f27539d3fa975ff27fe
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-05-03 15:46:20 -0500

    Create btn and add event listner to btn

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,7 @@ const fooTask = Task();
 fooTask.addTitle("Clean");
 console.log(fooTask.getTitle());
 fooTask.addDescription("Clean House");
+
+const subBtn = document.querySelector("#test");
+
+subBtn.addEventListener("click", () => console.log("hello"));
