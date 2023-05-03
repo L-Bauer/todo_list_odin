b318c033443b48a04f650f1db7a217bf03cc6b01
Lucas Bauer <lucas.d.bauer95@gmail.com>
2023-05-03 18:22:17 -0500

    Remove test code

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,1 @@
 import Task from "./task";
-
-const fooTask = Task();
-fooTask.addTitle("Clean");
-console.log(fooTask.getTitle());
-fooTask.addDescription("Clean House");
-
-const subBtn = document.querySelector("#test");
-
-subBtn.addEventListener("click", () => {
-  fooTask.addTitle(prompt());
-  console.log(fooTask.getTitle());
-});
